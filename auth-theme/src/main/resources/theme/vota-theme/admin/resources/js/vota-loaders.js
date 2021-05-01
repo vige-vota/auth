f863a683c1f21c74ee8979efbad884b69d1205e7
Luca Stancapiano <lucastancapiano@192.168.1.2>
2021-05-01 19:06:41 +0200

    new theme

--- auth-theme/src/main/resources/theme/vota-theme/admin/resources/js/vota-loaders.js
+++
@@ -1,8 +0,0 @@
-module.factory('VotaLoader', function(Loader, Vota, $route, $q) {
-    return Loader.get(Vota, function() {
-        return {
-            realm : $route.current.params.realm,
-            votaId : $route.current.params.vota
-        }
-    });
-});
