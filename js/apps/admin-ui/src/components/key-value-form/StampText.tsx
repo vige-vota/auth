1c50d7c7462e3138f575581fe406d9dab47c0e2e
Luca Stancapiano <lucastancapiano@MacBook-Pro-4.local>
2023-08-10 15:51:38 +0200

    stamp print

--- js/apps/admin-ui/src/components/key-value-form/StampText.tsx
+++ js/apps/admin-ui/src/components/key-value-form/StampText.tsx
@@ -1,5 +1,3 @@
-import styles from "@patternfly/react-styles/css/components/FormControl/form-control";
-import { css } from "@patternfly/react-styles";
 import { Component, createRef, forwardRef } from "react";
 import {
   getDefaultOUIAId,
@@ -12,27 +10,12 @@ export interface StampTextProps
     OUIAProps {
   /** Additional classes added to the text input. */
   className?: string;
-  /** Flag to show if the text input is disabled. */
-  isDisabled?: boolean;
-  /** @deprecated Use readOnlyVariant instead. Flag to show if the text input is read only. */
-  isReadOnly?: boolean;
-  /** Read only variant. */
-  readOnlyVariant?: "plain" | "default";
-  isRequired?: boolean;
   /** Value of the text input. */
   value?: string | number;
   /** Aria-label. The text input requires an associated id or aria-label. */
   "aria-label"?: string;
   /** A reference object to attach to the text input box. */
   innerRef?: React.RefObject<any>;
-  /** icon variant */
-  iconVariant?: "calendar" | "clock" | "search";
-  /** Use the external file instead of a data URI */
-  isIconSprite?: boolean;
-  /** Custom icon url to set as the text input"s background-image */
-  customIconUrl?: string;
-  /** Dimensions for the custom icon set as the text input"s background-size */
-  customIconDimensions?: string;
   /** Value to overwrite the randomly generated data-ouia-component-id.*/
   ouiaId?: number | string;
   /** Set the value of data-ouia-safe. Only set to true when the component is in a static state, i.e. no animations are occurring. At all other times, this value must be false. */
@@ -48,10 +31,6 @@ export class StampTextBase extends Component<StampTextProps, StampTextState> {
   static defaultProps: StampTextProps = {
     "aria-label": "",
     className: "",
-    isRequired: false,
-    isDisabled: false,
-    isReadOnly: false,
-    isIconSprite: false,
     ouiaSafe: true,
   };
   inputRef = createRef<HTMLInputElement>();
@@ -79,52 +58,15 @@ export class StampTextBase extends Component<StampTextProps, StampTextState> {
   };
 
   render() {
-    const {
-      innerRef,
-      className,
-      value,
-      isReadOnly,
-      readOnly,
-      readOnlyVariant,
-      isRequired,
-      isDisabled,
-      isIconSprite,
-      iconVariant,
-      customIconUrl,
-      customIconDimensions,
-      ouiaId,
-      ouiaSafe,
-      ...props
-    } = this.props;
-
-    const customIconStyle = {} as any;
-    if (customIconUrl) {
-      customIconStyle.backgroundImage = `url("${customIconUrl}")`;
-    }
-    if (customIconDimensions) {
-      customIconStyle.backgroundSize = customIconDimensions;
-    }
+    const { innerRef, className, value, ouiaId, ouiaSafe, ...props } =
+      this.props;
 
     return (
       <input
         {...props}
-        className={css(
-          styles.formControl,
-          isIconSprite && styles.modifiers.iconSprite,
-          readOnlyVariant === "plain" && styles.modifiers.plain,
-          ((iconVariant && iconVariant !== "search") || customIconUrl) &&
-            styles.modifiers.icon,
-          iconVariant && styles.modifiers[iconVariant],
-          className
-        )}
+        className={css(styles.formControl, className)}
         value={this.sanitizeInputValue(value)}
-        required={isRequired}
-        disabled={isDisabled}
-        readOnly={!!readOnlyVariant || isReadOnly || readOnly}
         ref={innerRef || this.inputRef}
-        {...((customIconUrl || customIconDimensions) && {
-          style: customIconStyle,
-        })}
         {...getOUIAProps(
           "StampText",
           ouiaId !== undefined ? ouiaId : this.state.ouiaStateId,
