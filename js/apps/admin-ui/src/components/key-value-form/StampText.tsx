b94c033469ca7524eaabc9187ba9d32961f23e29
Luca Stancapiano <lucastancapiano@MacBook-Pro-4.local>
2023-08-11 11:14:34 +0200

    stamp print

--- js/apps/admin-ui/src/components/key-value-form/StampText.tsx
+++ js/apps/admin-ui/src/components/key-value-form/StampText.tsx
@@ -4,7 +4,7 @@ export interface StampTextProps {
   /** Value of the text input. */
   value?: string | number;
   /** A reference object to attach to the text input box. */
-  innerRef?: React.Ref<HTMLInputElement>;
+  innerRef?: React.Ref<HTMLOutputElement>;
 }
 
 export class StampTextBase extends Component<StampTextProps> {
@@ -13,12 +13,16 @@ export class StampTextBase extends Component<StampTextProps> {
   render() {
     const { innerRef, value, ...props } = this.props;
 
-    return <input {...props} value={value} ref={innerRef} />;
+    return (
+      <output {...props} ref={innerRef}>
+        {value}
+      </output>
+    );
   }
 }
 
 export const StampText = forwardRef(
-  (props: StampTextProps, ref: React.Ref<HTMLInputElement>) => (
+  (props: StampTextProps, ref: React.Ref<HTMLOutputElement>) => (
     <StampTextBase {...props} innerRef={ref} />
   )
 );
