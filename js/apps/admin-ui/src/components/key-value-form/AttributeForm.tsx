96de07ca05ad4390a37e309136c8539b957cda9f
Luca Stancapiano <lucastancapiano@MacBook-Pro-4.local>
2023-08-28 20:52:27 +0200

    circumscriptions

--- js/apps/admin-ui/src/components/key-value-form/AttributeForm.tsx
+++ js/apps/admin-ui/src/components/key-value-form/AttributeForm.tsx
@@ -14,24 +14,15 @@ import { useTranslation } from "react-i18next";
 
 import { FormAccess } from "../form-access/FormAccess";
 import type { KeyValueType } from "./key-value-convert";
+import { Cities } from "./Cities";
 import { KeyValueInput } from "./KeyValueInput";
 import { HelpItem } from "ui-shared";
 import { useState, ReactElement, Fragment } from "react";
 import {
-  ZonesFieldRepresentation,
-  level0,
-  level1,
-  level2,
-  level3,
-  level0value,
-  level1value,
-  level2value,
-  level3value,
-  initLocations,
-} from "../../context/cities/CitiesProvider";
-import {
   BlocksFieldRepresentation,
+  VotingPaperRepresentation,
   blocksLevel,
+  blockvalue,
   initBlocks,
 } from "../../context/blocks/BlocksProvider";
 import "./attribute-form.css";
@@ -47,36 +38,15 @@ export type AttributesFormProps = {
   fineGrainedAccess?: boolean;
 };
 
-const levelOptions = (
-  locations: ZonesFieldRepresentation[],
-  value: ZonesFieldRepresentation
-) => {
-  const options: ReactElement[] = [];
-  locations.map((location: ZonesFieldRepresentation) => {
-    options.push(
-      <SelectOption selected={location === value} value={location.id}>
-        {location.name}
-      </SelectOption>
-    );
-  });
-  return options;
-};
-
 const blockOptions = (
-  translation: any,
-  blocks: BlocksFieldRepresentation[],
-  value: string
+  blocks: VotingPaperRepresentation[],
+  value?: BlocksFieldRepresentation
 ) => {
   const options: ReactElement[] = [];
-  blocks.map((block: BlocksFieldRepresentation) => {
-    let idx = 0;
+  blocks.map((block: VotingPaperRepresentation) => {
     options.push(
-      <SelectOption
-        selected={block.name === value}
-        key={`blocksLevel-${idx++}`}
-        value={block.name}
-      >
-        {translation(`${block.name}`)}
+      <SelectOption selected={block === value} value={block.id}>
+        {block.name}
       </SelectOption>
     );
   });
@@ -96,18 +66,10 @@ export const AttributesForm = ({
     formState: { isDirty },
     handleSubmit,
   } = form;
-  const [level0Open, setLevel0Open] = useState(false);
-  const [level1Open, setLevel1Open] = useState(false);
-  const [level2Open, setLevel2Open] = useState(false);
-  const [level3Open, setLevel3Open] = useState(false);
   const [blocksOpen, setBlocksOpen] = useState(false);
-  const locations = initLocations();
+  let valueFromRender: VotingPaperRepresentation;
   const blocks = initBlocks();
   const { t } = useTranslation("users");
-  let valueFromRender0: ZonesFieldRepresentation;
-  let valueFromRender1: ZonesFieldRepresentation;
-  let valueFromRender2: ZonesFieldRepresentation;
-  let valueFromRender3: ZonesFieldRepresentation;
 
   return (
     <FormAccess
@@ -133,201 +95,31 @@ export const AttributesForm = ({
             name="attributes"
             defaultValue=""
             render={({ field }) => {
-              const valueFromRender = field.value[0];
-              if (valueFromRender !== undefined) {
-                return (
-                  <Select
-                    toggleId="kc-blocks"
-                    onToggle={() => setBlocksOpen(!blocksOpen)}
-                    onSelect={(_, value) => {
-                      field.onChange(value as string);
-                      setBlocksOpen(false);
-                    }}
-                    selections={valueFromRender}
-                    variant={SelectVariant.single}
-                    aria-label={t("blocks")}
-                    isOpen={blocksOpen}
-                    placeholderText={t("users-help:blocks-ph")}
-                    data-testid="select-blocks"
-                  >
-                    {blockOptions(t, blocksLevel(blocks), valueFromRender)}
-                  </Select>
-                );
-              } else return <output>buuuuuublocks</output>;
+              const fieldValue = field.value;
+              valueFromRender = blockvalue(fieldValue, blocks);
+              return (
+                <Select
+                  toggleId="kc-blocks"
+                  onToggle={() => setBlocksOpen(!blocksOpen)}
+                  onSelect={(_, value) => {
+                    field.onChange(value as string);
+                    setBlocksOpen(false);
+                  }}
+                  selections={valueFromRender}
+                  variant={SelectVariant.single}
+                  aria-label={t("blocks")}
+                  isOpen={blocksOpen}
+                  placeholderText={t("users-help:blocks-ph")}
+                  data-testid="select-blocks"
+                >
+                  {blockOptions(blocksLevel(blocks))}
+                </Select>
+              );
             }}
           />
         </FormGroup>
 
-        <fieldset className="border-top">
-          <legend>
-            <span className="text">{t("users:residence")}</span>
-          </legend>
-          <FormGroup
-            label={t("level0")}
-            fieldId="kc-level0"
-            labelIcon={
-              <HelpItem
-                helpText={t("users-help:level0")}
-                fieldLabelId="users:level0"
-              />
-            }
-          >
-            <Controller
-              name="attributes"
-              defaultValue=""
-              render={({ field }) => {
-                const fieldValue = field.value;
-                valueFromRender0 = level0value(fieldValue, locations);
-                return (
-                  <Select
-                    toggleId="kc-level0"
-                    onToggle={() => {
-                      setLevel0Open(!level0Open);
-                    }}
-                    onSelect={(_, value) => {
-                      field.onChange(value as string);
-                      setLevel0Open(false);
-                    }}
-                    selections={valueFromRender0}
-                    variant={SelectVariant.single}
-                    aria-label={t("level0")}
-                    isOpen={level0Open}
-                    placeholderText={t("users-help:level0-ph")}
-                    data-testid="select-level0"
-                  >
-                    {levelOptions(level0(locations), valueFromRender0)}
-                  </Select>
-                );
-              }}
-            />
-          </FormGroup>
-          <FormGroup
-            label={t("level1")}
-            fieldId="kc-level1"
-            labelIcon={
-              <HelpItem
-                helpText={t("users-help:level1")}
-                fieldLabelId="users:level1"
-              />
-            }
-          >
-            <Controller
-              name="attributes"
-              defaultValue=""
-              render={({ field }) => {
-                const fieldValue = field.value;
-                valueFromRender1 = level1value(fieldValue, locations);
-                return (
-                  <Select
-                    toggleId="kc-level1"
-                    onToggle={() => {
-                      setLevel1Open(!level1Open);
-                    }}
-                    onSelect={(_, value) => {
-                      field.onChange(value as string);
-                      setLevel1Open(false);
-                    }}
-                    selections={valueFromRender1}
-                    variant={SelectVariant.single}
-                    aria-label={t("level1")}
-                    isOpen={level1Open}
-                    placeholderText={t("users-help:level1-ph")}
-                    data-testid="select-level1"
-                  >
-                    {levelOptions(
-                      level1(locations, valueFromRender0),
-                      valueFromRender1
-                    )}
-                  </Select>
-                );
-              }}
-            />
-          </FormGroup>
-          <FormGroup
-            label={t("level2")}
-            fieldId="kc-level2"
-            labelIcon={
-              <HelpItem
-                helpText={t("users-help:level2")}
-                fieldLabelId="users:level2"
-              />
-            }
-          >
-            <Controller
-              name="attributes"
-              defaultValue=""
-              render={({ field }) => {
-                const fieldValue = field.value;
-                valueFromRender2 = level2value(fieldValue, locations);
-                return (
-                  <Select
-                    toggleId="kc-level2"
-                    onToggle={() => {
-                      setLevel2Open(!level2Open);
-                    }}
-                    onSelect={(_, value) => {
-                      field.onChange(value as string);
-                      setLevel2Open(false);
-                    }}
-                    selections={valueFromRender2}
-                    variant={SelectVariant.single}
-                    aria-label={t("level2")}
-                    isOpen={level2Open}
-                    placeholderText={t("users-help:level2-ph")}
-                    data-testid="select-level2"
-                  >
-                    {levelOptions(
-                      level2(locations, valueFromRender1),
-                      valueFromRender2
-                    )}
-                  </Select>
-                );
-              }}
-            />
-          </FormGroup>
-          <FormGroup
-            label={t("level3")}
-            fieldId="kc-level3"
-            labelIcon={
-              <HelpItem
-                helpText={t("users-help:level3")}
-                fieldLabelId="users:level3"
-              />
-            }
-          >
-            <Controller
-              name="attributes"
-              defaultValue=""
-              render={({ field }) => {
-                const fieldValue = field.value;
-                valueFromRender3 = level3value(fieldValue, locations);
-                return (
-                  <Select
-                    toggleId="kc-level3"
-                    onToggle={() => {
-                      setLevel3Open(!level3Open);
-                    }}
-                    onSelect={(_, value) => {
-                      field.onChange(value as string);
-                      setLevel3Open(false);
-                    }}
-                    selections={valueFromRender3}
-                    variant={SelectVariant.single}
-                    aria-label={t("level3")}
-                    isOpen={level3Open}
-                    placeholderText={t("users-help:level3-ph")}
-                    data-testid="select-level3"
-                  >
-                    {levelOptions(
-                      level3(locations, valueFromRender2),
-                      valueFromRender3
-                    )}
-                  </Select>
-                );
-              }}
-            />
-          </FormGroup>
-        </fieldset>
+        <Cities />
 
         <fieldset className="border-top">
           <legend>
--- js/apps/admin-ui/src/context/blocks/BlocksProvider.tsx
+++ js/apps/admin-ui/src/context/blocks/BlocksProvider.tsx
@@ -10,7 +10,31 @@ export interface BlocksRepresentation {
 export interface BlocksFieldRepresentation {
   id?: string;
   name?: string;
-  votingPapers: BlocksFieldRepresentation[];
+  votingPapers: VotingPaperRepresentation[];
+}
+
+export interface VotingPaperRepresentation {
+  id?: string;
+  name?: string;
+  groups?: GroupRepresentation[];
+  parties?: PartyRepresentation[];
+}
+
+export interface GroupRepresentation {
+  id?: string;
+  name?: string;
+  parties?: PartyRepresentation[];
+}
+
+export interface PartyRepresentation {
+  id?: string;
+  name?: string;
+  candidates?: CandidateRepresentation[];
+}
+
+export interface CandidateRepresentation {
+  id?: string;
+  name?: string;
 }
 
 export const initBlocks = () => {
@@ -41,6 +65,77 @@ export const initBlocks = () => {
   return blocks;
 };
 
-export const blocksLevel = (blocks: BlocksRepresentation) => {
-  return blocks.votingPapers;
+const flatVotingPapers: VotingPaperRepresentation[] = [];
+
+export const blockvalue = (
+  block: any,
+  blocks: BlocksRepresentation
+): VotingPaperRepresentation => {
+  let level: VotingPaperRepresentation = block;
+  const levels: VotingPaperRepresentation[] = blocksLevel(blocks);
+  if (Array.isArray(location)) {
+    const valueFromRenderAll = block[1];
+    const value = `${Object.values(valueFromRenderAll)[1]}`;
+    const id = value;
+    levels.forEach((block) => {
+      if (block.id === id) level = block;
+      block.toString = function locToString() {
+        return `${this.name}`;
+      };
+    });
+  } else if (typeof block === "string" && block !== "") {
+    const splittedBlock = block.split("-");
+    let id1: string | undefined = "";
+    if (splittedBlock.length >= 2)
+      id1 = splittedBlock[0] + "-" + splittedBlock[1];
+    const id = id1;
+    levels.forEach((loc) => {
+      if (loc.id === id) level = loc;
+      loc.toString = function locToString() {
+        return `${this.name}`;
+      };
+    });
+  }
+  return level;
+};
+
+export const blocksLevel = (
+  blocks: BlocksRepresentation
+): VotingPaperRepresentation[] => {
+  if (flatVotingPapers.length === 0) {
+    blocks.votingPapers.forEach((e: VotingPaperRepresentation) => {
+      flatVotingPapers.push(e);
+      if (e.groups) {
+        e.groups.forEach((f) => {
+          f.name = "------ " + f.name;
+          flatVotingPapers.push(f);
+          if (f.parties) {
+            f.parties.forEach((h) => {
+              h.name = "------------------------ " + h.name;
+              flatVotingPapers.push(h);
+              if (h.candidates) {
+                h.candidates.forEach((i) => {
+                  i.name = "------------------------------------ " + i.name;
+                  flatVotingPapers.push(i);
+                });
+              }
+            });
+          }
+        });
+      }
+      if (e.parties) {
+        e.parties.forEach((g) => {
+          g.name = "------ " + g.name;
+          flatVotingPapers.push(g);
+          if (g.candidates) {
+            g.candidates.forEach((l) => {
+              l.name = "------------------------ " + l.name;
+              flatVotingPapers.push(l);
+            });
+          }
+        });
+      }
+    });
+  }
+  return flatVotingPapers;
 };
