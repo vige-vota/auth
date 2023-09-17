d96300a1d6c0a3b28e0569e0ce7a47b053d332a3
Luca Stancapiano <luca.stancapiano@sourcesense.com>
2023-09-17 20:20:07 +0200

    keycloak upgrade

--- js/apps/admin-ui/src/user/utils.ts
+++
@@ -1,3 +0,0 @@
-export const isBundleKey = (displayName?: string) =>
-  displayName?.includes("${");
-export const unWrap = (key: string) => key.substring(2, key.length - 1);
--- js/apps/admin-ui/src/utils/useParams.ts
+++
@@ -1,4 +0,0 @@
-import { useParams as useParamsRR } from "react-router-dom";
-
-export const useParams = <T extends Record<string, string>>() =>
-  useParamsRR<T>() as T;
